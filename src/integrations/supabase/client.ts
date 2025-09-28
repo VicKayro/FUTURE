e939a94504814e622d67456efbf20dbd7c28cf26
VicKayro <155428217+VicKayro@users.noreply.github.com>
2025-09-28 15:46:58 +0200

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://qxnydksqgodxmeijjrza.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4bnlka3NxZ29keG1laWpqcnphIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg5NTkxNzMsImV4cCI6MjA3NDUzNTE3M30.Zx-kn52H-fiexYaQJxDKblyxO1Ai3Cg7VU0jycvZ04o";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL!;
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY!;
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
